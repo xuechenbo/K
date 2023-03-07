506c1c42df509bd2bb952c96a763cc428ecf356d
xuechenbo <1306765203@qq.com>
2023-03-07 16:07:15 +0800

    1

--- app/src/main/java/com/interview/com/java/KotlinJavaActivity.java
+++ app/src/main/java/com/interview/com/java/KotlinJavaActivity.java
@@ -4,30 +4,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.interview.com.R;
-import com.interview.com.serview.AAservice;
+import com.Handler.com.R;
+
 
 class KotlinJavaActivity extends AppCompatActivity {
+
+    private Button button;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, AAservice.class);
-        startService(intent);
+        setContentView(R.layout.activity_kot);
+        button = findViewById(R.id.btn);
+        init();
     }
 
+    public void init() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("TAG", "click");
+            }
+        });
 
-    class aaa{
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.e("TAG", "touchListener" + motionEvent.getAction());
+                return false;
+            }
+        });
 
-        public aaa() {
-        }
-        public aaa(String a) {
-        }
     }
-
 }
